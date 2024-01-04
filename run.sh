7198e65019708f83277b72860b58d2259af8372f
Ian Droogmans <ian.droogmans@gmail.com>
2024-01-04 11:41:29 +0100

    Update run.sh to match documentation

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@
 # echo $MQTT_USER
 # echo $MQTT_PWD
 
+# set default env variables
+: "${MQTT_PORT:=1883}"
+
+#start Gateway
 echo "Starting Qbus MQTT gateway."
 echo "Connecting to MQTT broker '$MQTT_HOST:$MQTT_PORT' with user '$MQTT_USER'."
 /usr/bin/qbus/qbusMqttGw -serial QBUSMQTTGW -logbuflevel -1 -logtostderr true -storagedir /opt/qbus -mqttbroker "tcp://$MQTT_HOST:$MQTT_PORT" -mqttuser "$MQTT_USER" -mqttpassword "$MQTT_PWD"
